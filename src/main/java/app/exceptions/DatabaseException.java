afeff8fcf9102b9ae94eddb2e8b6a9d05383d5aa
Johan <179227533+JosefBenjamin@users.noreply.github.com>
2025-05-03 16:16:09 +0200

    added favicon images to display in browser, created tempalte html pages for now, adjust the formatting as well

--- src/main/java/app/exceptions/DatabaseException.java
+++ src/main/java/app/exceptions/DatabaseException.java
@@ -1,14 +1,12 @@
 package app.exceptions;
 
-public class DatabaseException extends Exception{
+public class DatabaseException extends Exception {
 
 
     /**
-     *
      * @param userMsg is a custom message meant to explain the error to the end-user
      *                userMsg is saved in the Exception superclass constructor,
      *                it can be accessed by using e.getMessage();
-     *
      */
 
     public DatabaseException(String userMsg) {
@@ -16,10 +14,8 @@ public class DatabaseException extends Exception{
     }
 
     /**
-     *
      * @param devMsg is a custom message meant to explain the error to a fellow developer
-     *                devMsg is just printed out to the console.
-     *
+     *               devMsg is just printed out to the console.
      */
 
     public DatabaseException(String userMsg, String devMsg) {
@@ -28,11 +24,9 @@ public class DatabaseException extends Exception{
     }
 
     /**
-     *
-     * @param userMsg is a custom message meant to explain the error to the end-user
-     *                userMsg is saved in the Exception superclass constructor,
-     *                it can be accessed by using e.getMessage();
-     *
+     * @param userMsg   is a custom message meant to explain the error to the end-user
+     *                  userMsg is saved in the Exception superclass constructor,
+     *                  it can be accessed by using e.getMessage();
      * @param rootCause finds the specific exception, from which all inherit or extend from Throwable
      */
 
